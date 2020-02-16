f0a413791acf44e207dee7f0f119d5a6b743dd47
Johan Steenhoven <sbkg0002@gmail.com>
2020-02-16 12:41:51 +0100

    Redo structure

--- prometheus_esmr5/main.py
+++ prometheus_esmr5/main.py
@@ -1,5 +1,6 @@
 from prometheus_client import start_http_server, Summary, Gauge
 import time
+import datetime
 import sys
 import serial
 
@@ -8,12 +9,7 @@ Inspired by:
 * https://github.com/sanderjo/P1/blob/master/P1-parser.awk
 * https://github.com/prometheus/client_python
 * https://github.com/gejanssen/slimmemeter-rpi
-'''
 
-# Metric configuration
-INTERVAL = 10
-
-# Only iterate over the following data types
 filter_list = {'1-0:1.8.1':'p1_total_electricity_used_rate_1',
             '1-0:1.8.2':'p1_total_electricity_used_rate_2',
             '1-0:2.8.1':'p1_total_electricity_provided_rate_1',
@@ -21,6 +17,16 @@ filter_list = {'1-0:1.8.1':'p1_total_electricity_used_rate_1',
             '1-0:1.7.0':'p1_total_electricity_used',
             '1-0:2.7.0':'p1_total_electricity_provided',
             '0-1:24.2.1(191204215508W)':'p1_total_gas_used'}
+'''
+
+# Define metrics
+p1_total_electricity_used_rate_1 = Gauge('p1_total_electricity_used_rate_1', 'bla')
+p1_total_electricity_used_rate_2 = Gauge('p1_total_electricity_used_rate_2', 'bla')
+p1_total_electricity_provided_rate_1 = Gauge('p1_total_electricity_provided_rate_1', 'bla')
+p1_total_electricity_provided_rate_2 = Gauge('p1_total_electricity_provided_rate_2', 'bla')
+p1_total_electricity_used = Gauge('p1_total_electricity_used', 'bla')
+p1_total_electricity_provided = Gauge('p1_total_electricity_provided', 'bla')
+p1_total_gas_used = Gauge('p1_total_gas_used', 'bla')
 
 # Serial configuration
 ser = serial.Serial()
@@ -33,81 +39,65 @@ ser.rtscts=0
 ser.timeout=20
 ser.port="/dev/ttyUSB0"
 
-# Open COM port
-try:
-    ser.open()
-except:
-    sys.exit ("Fout bij het openen van %s."  % ser.name)      
+# Interval in seconds
+interval = 10
 
-def get_p1_statistics():
-    # Initialize
+
+def get_p1_metrics():
+    ser_open()
+
     p1_counter=0
-    p1_state = {}
-
     while p1_counter < 26:
+        p1_counter = p1_counter +1
         p1_line=''
         try:
             p1_raw = ser.readline()
         except:
-            sys.exit ("Seriele port %s cannot be read. Exiting." % ser.name )    
+            sys.exit ("Seriele port %s cannot be read. Exiting." % ser.name )
 
         # Generate a clean string per line
-        p1_str=str(p1_raw, "utf-8").strip()
+        p1_line=str(p1_raw, "utf-8").strip()
 
-        # Only lines starting with 0 or 1
-        if p1_str[0:1] in ['0','1']:
-            # Split the lines on '('
-            p1_list = p1_str.split('(')
+        if '1-0:1.8.1' in p1_line:
+            # print("p1_total_electricity_used_rate_1: {}" .format(markup_helper(p1_line)))
+            p1_total_electricity_used_rate_1.set(markup_helper(p1_line))
+        elif '1-0:1.8.2' in p1_line:
+            # print("p1_total_electricity_used_rate_2: {}" .format(markup_helper(p1_line)))
+            p1_total_electricity_used_rate_2.set(markup_helper(p1_line))
+        elif '1-0:2.8.1' in p1_line:
+            # print("p1_total_electricity_provided_rate_1: {}" .format(markup_helper(p1_line)))
+            p1_total_electricity_provided_rate_1.set(markup_helper(p1_line))
 
-            # Only use the types in filter_list
-            if p1_list[0] in filter_list:
-                # Add key:value for metric_name:value
-                # Get the friendly name from the filter list and cut off the '*kWh'
-                p1_state[filter_list.get(p1_list[0])] = p1_list[1].split('*')[0]
+    ser.close()
 
-        # if p1_str[0:9] == '0-1:24.2.1':
-            # print("YES")
 
-        p1_counter = p1_counter +1
 
+def ser_open():
+    # Open COM port
+    try:
+        ser.open()
+    except:
+        sys.exit ("Error opening %s."  % ser.name)      
+
+def ser_close():
     #Close port and show status
     try:
         ser.close()
     except:
-        sys.exit ("Oops %s. Programma afgebroken." % ser.name )      
+        sys.exit ("Oops %s. Programme aborted." % ser.name )      
 
-    return p1_state
-'''
-/1-0:1.8.1/ { print "Electra: Totaal verbruik tarief 1 (nacht) KWh: " getfloat($0) }
-/1-0:1.8.2/ { print "Electra: Totaal verbruik tarief 2 (dag) KWh: "   getfloat($0) }
-/1-0:2.8.1/ { print "Electra: Totaal geleverd tarief 1 (nacht) KWh: " getfloat($0) }
-/1-0:2.8.2/ { print "Electra: Totaal geleverd tarief 2 (dag) KWh: "   getfloat($0) }
+def markup_helper(str_line):
+    '''
+    Read raw string and return only the value
+    '''
+    return str_line.split('(')[1].split('*')[0]
 
-/1-0:1.7.0/ { print "Electra: Huidig verbruik [W]: "             1000*getfloat($0) }
-/1-0:2.7.0/ { print "Electra: Huidige teruglevering [W]: "       1000*getfloat($0) }
-
-/^\(/       { print "Gas: Totaal verbruik gas in m3: "                getfloat($0) }
-'''
-    
-
-
-def process_request(t):
-    """A dummy function that takes some time."""
-    g = Gauge('my_inprogress_requests', 'Description of gauge')
-    time.sleep(t)
 
 if __name__ == '__main__':
-    # Define gauges
-    # https://github.com/prometheus/client_python#gauge
-    for metric, usage in get_p1_statistics().items():
-        g = Gauge(metric, 'Description of gauge')
-        g.set(usage)
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
-        process_request(INTERVAL)
-
-# for metric, usage in get_p1_statistics().items():
-#     print(metric)
-#     print(usage)
+        get_p1_metrics()
+        print("[{}] Got new metrics from device." .format(datetime.datetime.now()))
+        time.sleep(interval)
