cb8df83ee7afebffdd3968ae297b891a5777684d
Johan Steenhoven <sbkg0002@gmail.com>
2020-02-17 20:24:55 +0100

    Use meterd

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Prometheus exporter for ESMR 5.0
-Smart Meter ESMR 5.0 Prometheus exporter
+> Smart Meter ESMR 5.0 Prometheus exporter
 
 Currently for arm32v7 only.
 
 To run in Docker:
 ```
-docker run --detach --name prometheus_esmr5 --publish 8000:8000 --device=/dev/ttyUSB0 sbkg0002/prometheus_esmr5:arm32v7-latest
+docker run \
+    --detach \
+    --name prometheus_esmr5 \
+    --publish 8000:8000 \
+    --device=/dev/ttyUSB0 \
+    sbkg0002/prometheus_esmr5:arm32v7-latest
 ```
+To view the logs:
+```
+[2020-02-17 19:22:31.838974] p1_total_electricity_used_rate_1: 460935.0
+[2020-02-17 19:22:31.839388] p1_total_electricity_used_rate_2: 417854.0
+[2020-02-17 19:22:31.839715] p1_total_electricity_provided_rate_1: 0.0
+[2020-02-17 19:22:31.840107] p1_total_electricity_used: 275.0
+[2020-02-17 19:22:31.840514] p1_total_electricity_provided: 0.0
+[2020-02-17 19:22:31.841802] p1_total_gas_used: 798077.0
+````
 
 You can view the metrics at http://localhost:8000/metrics
--- prometheus_esmr5/main.py
+++ prometheus_esmr5/main.py
@@ -11,13 +11,13 @@ Inspired by:
 * https://github.com/prometheus/client_python
 * https://github.com/gejanssen/slimmemeter-rpi
 
-filter_list = {'1-0:1.8.1':'p1_total_electricity_used_rate_1',
-            '1-0:1.8.2':'p1_total_electricity_used_rate_2',
-            '1-0:2.8.1':'p1_total_electricity_provided_rate_1',
-            '1-0:2.8.2':'p1_total_electricity_provided_rate_2',
-            '1-0:1.7.0':'p1_total_electricity_used',
-            '1-0:2.7.0':'p1_total_electricity_provided',
-            '0-1:24.2.1(191204215508W)':'p1_total_gas_used'}
+'1-0:1.8.1':'p1_total_electricity_used_rate_1',
+'1-0:1.8.2':'p1_total_electricity_used_rate_2',
+'1-0:2.8.1':'p1_total_electricity_provided_rate_1',
+'1-0:2.8.2':'p1_total_electricity_provided_rate_2',
+'1-0:1.7.0':'p1_total_electricity_used',
+'1-0:2.7.0':'p1_total_electricity_provided',
+'0-1:24.2.1(191204215508W)':'p1_total_gas_used'
 '''
 
 # Define metrics
@@ -30,51 +30,38 @@ p1_total_electricity_provided = Gauge('p1_total_electricity_provided', 'Totaal g
 p1_total_gas_used = Gauge('p1_total_gas_used', 'Totaal gebruik gas')
 
 # Interval in seconds
-interval = 30
+interval = 15
 
-# Define device
-meter = SmartMeter('/dev/ttyUSB0', baudrate=115200)
-
-def get_p1_metrics():
-
-    try:
-        p1_lines = meter.read_one_packet()
-    except:
-        sys.exit ("Seriele port %s cannot be read. Exiting." % ser.name )
-
+def get_p1_metrics(p1_lines):
+    # Convert to a list for simple parsing
     p1_list = str(p1_lines).splitlines()
-    print(p1_list)
-    print(len(p1_list))
-    for p1_line in p1_lines:
 
-        print(p1_line)
-        # # Generate a clean string per line
-        # p1_line=str(p1_raw, "utf-8").strip()
-
+    for p1_line in p1_list:
+        # Parse per line
         if '1-0:1.8.1' in p1_line:
-            print("p1_total_electricity_used_rate_1: {}" .format(markup_helper(p1_line)))
+            print("[{}] p1_total_electricity_used_rate_1: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
             p1_total_electricity_used_rate_1.set(markup_helper(p1_line))
         elif '1-0:1.8.2' in p1_line:
-            print("p1_total_electricity_used_rate_2: {}" .format(markup_helper(p1_line)))
+            print("[{}] p1_total_electricity_used_rate_2: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
             p1_total_electricity_used_rate_2.set(markup_helper(p1_line))
         elif '1-0:2.8.1' in p1_line:
-            print("p1_total_electricity_provided_rate_1: {}" .format(markup_helper(p1_line)))
+            print("[{}] p1_total_electricity_provided_rate_1: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
             p1_total_electricity_provided_rate_1.set(markup_helper(p1_line))
         elif '1-0:1.7.0' in p1_line:
-            print("p1_total_electricity_used: {}" .format(markup_helper(p1_line)))
+            print("[{}] p1_total_electricity_used: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
             p1_total_electricity_used.set(markup_helper(p1_line))
         elif '1-0:2.7.0' in p1_line:
-            print("p1_total_electricity_provided: {}" .format(markup_helper(p1_line)))
+            print("[{}] p1_total_electricity_provided: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
             p1_total_electricity_provided.set(markup_helper(p1_line))
         elif '0-1:24.2.1' in p1_line:
-            print("p1_total_gas_used: {}" .format(markup_helper(p1_line)))
+            print("[{}] p1_total_gas_used: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
             p1_total_gas_used.set(markup_helper(p1_line))
 
 def markup_helper(str_line):
     '''
     Read raw string and return only the value
     '''
-    return float(str_line.split('(')[-1].split('*')[0])
+    return int(str_line.split('(')[-1].split('*')[0].replace('.',''))
 
 
 if __name__ == '__main__':
@@ -82,6 +69,12 @@ if __name__ == '__main__':
     start_http_server(8000)
     # Generate some requests.
     while True:
-        get_p1_metrics()
-        print("[{}] Got new metrics from device." .format(datetime.datetime.now()))
+        try:
+            # Define device
+            meter = SmartMeter('/dev/ttyUSB0', baudrate=115200)
+            get_p1_metrics(meter.read_one_packet())
+            meter.disconnect()
+        except:
+            sys.exit ("Serial port cannot be read. Will try again.")
+        print("")
         time.sleep(interval)
